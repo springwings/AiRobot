165698ca7ef76f5d3f48dd03d2432cba1f526c42
成文 <seo@5lulu.com>
2023-10-09 09:52:20 +0800

    Update TTS.py

--- src/Service/TTS.py
+++ src/Service/TTS.py
@@ -5,10 +5,7 @@ tts
 
 Author: chengwen
 Modifier:
-date:   2023/3/13 上午11:43
-Description: 
-Copyright:  Fujian Linewell Software Co., Ltd. All rights reserved.
-注意：本内容仅限于福建南威软件股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
+date:   2023/3/13 上午11:43 
 """
 import os
 import random
